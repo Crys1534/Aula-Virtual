f41d84a52168f02ebb187f294a27a7c50f7222f2
Crystal <crystalsnow1534@gmail.com>
2026-04-27 21:26:00 -0600

    Alpha 0.7

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnX__R6A6lQw_KuzZxqHRkzZyj10S_4wU",
+  authDomain: "aula-virtual-79e4d.firebaseapp.com",
+  projectId: "aula-virtual-79e4d",
+  storageBucket: "aula-virtual-79e4d.firebasestorage.app",
+  messagingSenderId: "384731632952",
+  appId: "1:384731632952:web:9b4facff98bc7f6ebf5dcd",
+  measurementId: "G-SR9D2CJX40"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
